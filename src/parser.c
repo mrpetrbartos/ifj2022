1fc612894ec001a2171b321ddf8778e7d9f20dda
Petr Bartoš <mrpetrbartos@gmail.com>
2022-11-27 19:54:48 +0100

    Post trial submission adjustments

--- src/parser.c
+++ src/parser.c
@@ -1,7 +1,7 @@
 /**
  * @file parser.c
  * @author Petr Bartoš (xbarto0g)
- * @brief Top-down token parser.
+ * @brief Recursive descent parser.
  */
 
 #include "parser.h"
@@ -30,10 +30,57 @@ void parserDestroy()
     free(parser.stack);
 }
 
+int checkPrologue()
+{
+    Token prologue[7] = {{.type = TOKEN_IDENTIFIER_FUNC, .value.string.content = "declare"},
+                         {.type = TOKEN_LEFT_BRACKET},
+                         {.type = TOKEN_IDENTIFIER_FUNC, .value.string.content = "strict_types"},
+                         {.type = TOKEN_ASSIGN},
+                         {.type = TOKEN_INT, .value.integer = 1},
+                         {.type = TOKEN_RIGHT_BRACKET},
+                         {.type = TOKEN_SEMICOLON}};
+
+    Token t;
+    for (int i = 0; i < 8; i++)
+    {
+        getToken(&t);
+        if (t.type != prologue[i].type)
+            return 1;
+        if (t.type == TOKEN_IDENTIFIER_FUNC)
+        {
+            if (strcmp(t.value.string.content, prologue[i].value.string.content))
+            {
+                vStrFree(&t.value.string);
+                return 1;
+            }
+            vStrFree(&t.value.string);
+        }
+        if (t.type == TOKEN_INT)
+        {
+            if (t.value.integer != prologue[i].value.integer)
+                return 1;
+        }
+    }
+
+    return 0;
+}
+
 int parse()
 {
-    int parsingCode = parseExpression();
-    if (parsingCode != 0)
-        return parsingCode;
+    int errCode = checkPrologue();
+    if (errCode != 0)
+    {
+        parserDestroy();
+        return errCode;
+    }
+    // Insert builtin functions
+    symtableAdd(parser.symtable, "floatval", FUNC, 1);
+    symtableAdd(parser.symtable, "intval", FUNC, 1);
+    symtableAdd(parser.symtable, "strval", FUNC, 1);
+    symtableAdd(parser.symtable, "strlen", FUNC, 1);
+    symtableAdd(parser.symtable, "substr", FUNC, 3);
+    symtableAdd(parser.symtable, "ord", FUNC, 1);
+    symtableAdd(parser.symtable, "chr", FUNC, 1);
+
     return 0;
 }
