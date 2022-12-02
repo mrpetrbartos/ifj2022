66da64aaab574757aaa797a5a46cee81e6476671
defy01 <rajsigl.tomas@gmail.com>
2022-12-02 23:16:57 +0100

    Added parser tests
    Basic tests mainly covering operators, if and while statements as well as some fundamental arithmetics

--- test/parser.cpp
+++ test/parser.cpp
@@ -145,6 +145,157 @@ TEST_F(ParserTest, NoEpilogue)
 
     TestSc(input, 0);
 }
+TEST_F(ParserTest, Arithmetics1)
+{
+    std::string input =
+        "<?php\n"
+        "declare(strict_types=1);"
+        "$a = 1.0 + 2e2 - 3.4e+9 * 4.1E-7 / 0.5;"
+        "return 0;";
+
+    TestSc(input, 0);
+}
+
+TEST_F(ParserTest, Arithmetics2)
+{
+    std::string input =
+        "<?php\n"
+        "declare(strict_types=1);"
+        "$a = 1 + 2 - 3 * 4 / 5;"
+        "return 0;";
+
+    TestSc(input, 0);
+}
+
+TEST_F(ParserTest, ArithmeticsComment)
+{
+    std::string input =
+        "<?php\n"
+        "declare(strict_types=1);"
+        "$a = 1 + 2 -/*comment in expr*/ 3 * 4 / 5;"
+        "return 0;";
+
+    TestSc(input, 0);
+}
+
+TEST_F(ParserTest, Operators)
+{
+    std::string input =
+        "<?php\n"
+        "declare(strict_types=1);"
+        "if (1 === 1)"
+        "{}"
+        "else"
+        "{}"
+        "if (1 !== 0)"
+        "{}"
+        "else"
+        "{}"
+        "if (1 < 5)"
+        "{}"
+        "else"
+        "{}"
+        "if (1 > 0)"
+        "{}"
+        "else"
+        "{}"
+        "if (1 >= 1)"
+        "{}"
+        "else"
+        "{}"
+        "if (1 <= 1)"
+        "{}"
+        "else"
+        "{}"
+        "return 0;";
+
+    TestSc(input, 0);
+}
+
+TEST_F(ParserTest, Concatenate)
+{
+    std::string input =
+        "<?php\n"
+        "declare(strict_types=1);"
+        "$var = \" a \" . \" b \";"
+        "return 0;";
+
+    TestSc(input, 0);
+}
+
+TEST_F(ParserTest, WrongExpression)
+{
+    std::string input =
+        "<?php\n"
+        "declare(strict_types=1);"
+        "$var = 1 * 2 2;"
+        "return 0;";
+
+    TestSc(input, 2);
+}
+
+TEST_F(ParserTest, WrongOperator)
+{
+    std::string input =
+        "<?php\n"
+        "declare(strict_types=1);"
+        "$var = 1 ** 2;"
+        "return 0;";
+
+    TestSc(input, 2);
+}
+
+TEST_F(ParserTest, WrongBrackets)
+{
+    std::string input =
+        "<?php\n"
+        "declare(strict_types=1);"
+        "$a = (((1 + 2) - ((3) * 4)) / 5));"
+        "$b = (\" a \" . \" b \";"
+        "if (1 === (1)"
+        "{}"
+        "else"
+        "{}"
+        "return 0;";
+
+    TestSc(input, 2);
+}
+
+TEST_F(ParserTest, Brackets)
+{
+    std::string input =
+        "<?php\n"
+        "declare(strict_types=1);"
+        "$a = (((1 + 2) - ((3) * 4)) / 5);"
+        "$b = (\" a \") . (\" b \");"
+        "if (1 === (1))"
+        "{}"
+        "else"
+        "{}"
+        "if ((1 !== 0))"
+        "{}"
+        "else"
+        "{}"
+        "return 0;";
+
+    TestSc(input, 0);
+}
+
+TEST_F(ParserTest, Variables)
+{
+    std::string input =
+        "<?php\n"
+        "declare(strict_types=1);"
+        "$varA = 1;"
+        "$varB = 2;"
+        "$varC = 3;"
+        "$varD = 4;"
+        "$varE = 5;"
+        "$result = $varA + ($varB - $varC) * $varD / ($varE);"
+        "return 0;";
+
+    TestSc(input, 0);
+}
 
 TEST_F(ParserTest, AssignmentExample1)
 {
@@ -174,6 +325,7 @@ TEST_F(ParserTest, AssignmentExample1)
         "}"
         "write(\"Vysledek je: \", $vysl, \"\\n\");"
         "}";
+
     TestSc(input, 0);
 }
 
@@ -204,6 +356,7 @@ TEST_F(ParserTest, AssignmentExample2)
         "} else {"
         "write(\"Chyba pri nacitani celeho cisla!\\n\");"
         "}";
+
     TestSc(input, 0);
 }
 
@@ -225,5 +378,218 @@ TEST_F(ParserTest, AssignmentExample3)
         "}"
         "} else { }"
         "?>";
+
+    TestSc(input, 0);
+}
+
+TEST_F(ParserTest, WrongFunction1)
+{
+    std::string input =
+        "<?php\n"
+        "declare(strict_types=1);"
+        "function fun () : void"
+        "{}"
+        "fun;"
+        "return 0;";
+
+    TestSc(input, 2);
+}
+
+TEST_F(ParserTest, WrongFunction2)
+{
+    std::string input =
+        "<?php\n"
+        "declare(strict_types=1);"
+        "function fun () : void"
+        "{}"
+        "fun(;"
+        "return 0;";
+
+    TestSc(input, 2);
+}
+
+TEST_F(ParserTest, WrongFunction3)
+{
+    std::string input =
+        "<?php\n"
+        "declare(strict_types=1);"
+        "function fun () : void"
+        "{}"
+        "function fun();"
+        "return 0;";
+
+    TestSc(input, 2);
+}
+
+TEST_F(ParserTest, WrongFunction4)
+{
+    std::string input =
+        "<?php\n"
+        "declare(strict_types=1);"
+        "function fun () : int"
+        "{"
+        "return 0;"
+        "}"
+        "$var fun();"
+        "return 0;";
+
+    TestSc(input, 2);
+}
+
+TEST_F(ParserTest, WrongFunction5)
+{
+    std::string input =
+        "<?php\n"
+        "declare(strict_types=1);"
+        "function fun () : int"
+        "{"
+        "return 0;"
+        "}"
+        "$var = ();"
+        "return 0;";
+
+    TestSc(input, 2);
+}
+
+TEST_F(ParserTest, WrongIf1)
+{
+    std::string input =
+        "<?php\n"
+        "declare(strict_types=1);"
+        "if (1 === 1)"
+        "$var = 0;"
+        "else"
+        "{}"
+        "return 0;";
+
+    TestSc(input, 2);
+}
+
+TEST_F(ParserTest, WrongIf2)
+{
+    std::string input =
+        "<?php\n"
+        "declare(strict_types=1);"
+        "if (1 === 1)"
+        "{}"
+        "else"
+        "$var = 0;"
+        "return 0;";
+
+    TestSc(input, 2);
+}
+
+TEST_F(ParserTest, WrongIf3)
+{
+    std::string input =
+        "<?php\n"
+        "declare(strict_types=1);"
+        "if"
+        "{}"
+        "else"
+        "{}"
+        "return 0;";
+
+    TestSc(input, 2);
+}
+
+TEST_F(ParserTest, WrongIf4)
+{
+    std::string input =
+        "<?php\n"
+        "declare(strict_types=1);"
+        "if ()"
+        "{}"
+        "else"
+        "{}"
+        "return 0;";
+
+    TestSc(input, 2);
+}
+
+TEST_F(ParserTest, WrongIf5)
+{
+    std::string input =
+        "<?php\n"
+        "declare(strict_types=1);"
+        "if ("
+        "{}"
+        "else"
+        "{}"
+        "return 0;";
+
+    TestSc(input, 2);
+}
+
+TEST_F(ParserTest, WrongReturn)
+{
+    std::string input =
+        "<?php\n"
+        "declare(strict_types=1);"
+        "return string;";
+
+    TestSc(input, 2);
+}
+
+TEST_F(ParserTest, WrongWhile1)
+{
+    std::string input =
+        "<?php\n"
+        "declare(strict_types=1);"
+        "while (1===1)"
+        "$var = 0;"
+        "return 0;";
+
+    TestSc(input, 2);
+}
+
+TEST_F(ParserTest, WrongWhile2)
+{
+    std::string input =
+        "<?php\n"
+        "declare(strict_types=1);"
+        "while"
+        "{}"
+        "return 0;";
+
+    TestSc(input, 2);
+}
+
+TEST_F(ParserTest, WrongWhile3)
+{
+    std::string input =
+        "<?php\n"
+        "declare(strict_types=1);"
+        "while ()"
+        "{}"
+        "return 0;";
+
+    TestSc(input, 2);
+}
+
+TEST_F(ParserTest, WrongWhile4)
+{
+    std::string input =
+        "<?php\n"
+        "declare(strict_types=1);"
+        "while )"
+        "{}"
+        "return 0;";
+
+    TestSc(input, 2);
+}
+
+TEST_F(ParserTest, While)
+{
+    std::string input =
+        "<?php\n"
+        "declare(strict_types=1);"
+        "while (1===0)"
+        "{"
+        "while (7)"
+        "{}"
+        "}"
+        "return 0;";
+
     TestSc(input, 0);
 }
