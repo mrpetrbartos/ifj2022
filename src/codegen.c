63efeaea8896ee9dcd3c42ac1cd33beea3909f68
Petr Bartoš <mrpetrbartos@gmail.com>
2022-12-04 19:27:12 +0100

    Added codegen file structure
    Co-authored-by: Lukas Zedek <lukas.zedekcz@gmail.com>

---
+++ src/codegen.c
@@ -0,0 +1,79 @@
+/**
+ * @file codegen.c
+ * @author Lukáš Zedek (xzedek03)
+ * @brief Code generator implementation.
+ */
+
+#include "stdio.h"
+#include "codegen.h"
+
+void printHead()
+{
+    printf(".IFJcode22\n");
+    printf("DEFVAR GF@funcname\n");
+    printf("MOVE GF@funcname string@\\000\n");
+}
+
+void genStackPush(Token t)
+{
+    switch (t.type)
+    {
+    case TOKEN_STRING:
+        printf("PUSHS string@%s\n", t.value.string.content);
+        vStrFree(&t.value.string);
+        break;
+
+    case TOKEN_INT:
+        printf("PUSHS int@%i\n", t.value.integer);
+        break;
+
+    case TOKEN_FLOAT:
+        printf("PUSHS float@%f\n", t.value.decimal);
+        break;
+
+    case TOKEN_KEYWORD:
+        printf("PUSHS nil@nil\n");
+        break;
+
+    case TOKEN_PLUS:
+        printf("ADDS\n");
+        break;
+
+    case TOKEN_MINUS:
+        printf("SUBS\n");
+        break;
+
+    case TOKEN_CONCAT:
+        printf("CONCAT\n");
+        break;
+
+    case TOKEN_MULTIPLY:
+        printf("MULS\n");
+        break;
+
+    case TOKEN_DIVIDE:
+        printf("DIVS\n");
+        break;
+
+    case TOKEN_GREATER_EQUAL:
+        printf("GTS\n");
+        printf("EQS\n");
+        break;
+
+    case TOKEN_LESS_EQUAL:
+        printf("LTS\n");
+        printf("EQS\n");
+        break;
+
+    case TOKEN_GREATER_THAN:
+        printf("GTS\n");
+        break;
+
+    case TOKEN_LESS_THAN:
+        printf("LTS\n");
+        break;
+
+    default:
+        break;
+    }
+}
---
+++ src/codegen.h
@@ -0,0 +1,25 @@
+/**
+ * @file codegen.h
+ * @author Lukáš Zedek (xzedek03)
+ * @brief Header file for code generator.
+ */
+
+#ifndef H_CODEGEN
+#define H_CODEGEN
+
+#include "scanner.h"
+
+/**
+ * @brief Prints info about used lang.
+ *
+ */
+void printHead();
+
+/**
+ * @brief Pushes token's value to stack.
+ *
+ * @param t Token whose value should be pushed.
+ */
+void genStackPush(Token t);
+
+#endif
